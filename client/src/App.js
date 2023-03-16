cb65040bb6ef8c6ca22d5391e7a339b7571ce0e4
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-03-16 21:20:16 +0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,47 +15,48 @@ function App() {
         setLoading(true)
 
         
-        let num = number;
-        if(!num.startsWith('+1')){
-            let pre = '+1'
-            num = pre.concat(num)
-        }
-        if(num.length !== 12){
-            setErrorMessage('Invalid phone number')
-            setError(true)
-            setLoading(false)
-            return;
-        }
+        let num = number.replace('/-/g', '');
+        console.log(num);
+        // if(!num.startsWith('+1')){
+        //     let pre = '+1'
+        //     num = pre.concat(num)
+        // }
+        // if(num.length !== 12){
+        //     setErrorMessage('Invalid phone number')
+        //     setError(true)
+        //     setLoading(false)
+        //     return;
+        // }
 
-        let data = {
-            number: num
-        }
-        const response = await fetch('/verifynumber',{
-            method: 'POST',
-            headers:{
-                'Content-Type': 'Application/json'
-            },
-            body: JSON.stringify(data)
-        })
+        // let data = {
+        //     number: num
+        // }
+        // const response = await fetch('/verifynumber',{
+        //     method: 'POST',
+        //     headers:{
+        //         'Content-Type': 'Application/json'
+        //     },
+        //     body: JSON.stringify(data)
+        // })
 
-        const res = await response.json()
+        // const res = await response.json()
 
-        if(response.status === 200){
-            if(res.num.carrier.type === "landline"){
-                setResponse("Invalid")
-            }
-            else if(res.num.carrier.mobile_network_code === null)
-                setResponse("Invalid")
-            else if(res.num.carrier.name === "AT&T Wireless" && res.num.carrier.mobile_country_code == "311" && res.num.carrier.mobile_network_code == "180" )
-                setResponse("Invalid")
-            else    
-                setResponse("Valid")
-        }
-        else{
-            setErrorMessage(res.message)
-            setError(true)
-        }
-        setLoading(false)
+        // if(response.status === 200){
+        //     if(res.num.carrier.type === "landline"){
+        //         setResponse("Invalid")
+        //     }
+        //     else if(res.num.carrier.mobile_network_code === null)
+        //         setResponse("Invalid")
+        //     else if(res.num.carrier.name === "AT&T Wireless" && res.num.carrier.mobile_country_code == "311" && res.num.carrier.mobile_network_code == "180" )
+        //         setResponse("Invalid")
+        //     else    
+        //         setResponse("Valid")
+        // }
+        // else{
+        //     setErrorMessage(res.message)
+        //     setError(true)
+        // }
+        // setLoading(false)
     }
   return (
       <>
